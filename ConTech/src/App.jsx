a2b285c1c822324b700714fd83ea9724c77d5521
hariansh-singh <harianshsingh13@gmail.com>
2024-07-18 23:45:34 +0530

    Setup jsx and css files

---
+++ ConTech/src/App.jsx
@@ -0,0 +1,16 @@
+import React from 'react';
+import List from './components/List/List';
+import Chat from './components/Chat/Chat';
+import Detail from './components/Detail/Detail';
+
+function App() {
+  return (
+    <div className='container'>
+      <List />
+      <Chat />
+      <Detail />
+    </div>
+  );
+}
+
+export default App;
